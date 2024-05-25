3d01d4d06ea37f4e418d8848ecd57d51ab5761fd
PetukhovIS <petukhov.is.96@ya.ru>
2024-05-25 16:40:23 +0600

    9.3. Математические операторы и составные операторы присваивания [#504905]

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -6,7 +6,7 @@ public class Greeting {
         System.out.println(idea);
         idea += "But I am a newbie.";
         int year = 2024;
-        idea = idea + year;
+        idea += year;
         System.out.println(idea);
     }
 }
