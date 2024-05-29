51313441db37b7de5bebcaca58958d4d6881bc9f
PetukhovIS <petukhov.is.96@ya.ru>
2024-05-29 18:08:20 +0600

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -6,9 +6,15 @@ public class ArrayDefinition {
         Short[] ages = new Short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Ilya Petukhov";
+        names[2] = "Ivan Ivanov";
+        names[3] = "Stas Peshkin";
         System.out.println("Размер массива ages равен: " + ages.length);
-        System.out.println("Размер массива surnames равен: " + surnames.length);
+        System.out.println("Размер массива surnames равен: " + names.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+        System.out.println(names[0] + ", " + names[1] + ", " + names[2] + ", " + names[3]);
 
     }
 }
