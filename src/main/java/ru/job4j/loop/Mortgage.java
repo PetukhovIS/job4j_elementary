9c2e126e22b216c06aa1e77e91ea7f2f0a5e6777
PetukhovIS <petukhov.is.96@ya.ru>
2024-05-30 18:09:37 +0600

    5.7. Ипотека [#156315 #498303]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -5,8 +5,8 @@ public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
         percent /= 100;
-        amount = (int) (amount + (amount * percent));
         while (amount > 0) {
+            amount += amount * percent;
             amount -= salary;
             year++;
         }
